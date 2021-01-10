ff90ee4de3eac9d84a27e69ab72b8af67e1db7c8
InfiniteCode <38706103+deneb1729@users.noreply.github.com>
2021-01-10 01:14:51 -0300

    Feature/deploy (#6)
    * feature/deploy - Completed tasks:
    
    - modified actions files
    - add config in settings
    - add necessary files for deploy on heroku
    
    * fixed pull actions
    
    * feature/deploy - Completed tasks:
    
    - Formater files (single quotes -> double quotes)
    - Fixed compressed variable for whitenoise
    
    * feature/deploy - Completed tasks:
    
    - implementation pre-commit hook
    - add configuration files for pre-commit hook
    
    * feature/deploy - Complete tasks:
    
    - modified github actions files
    
    * feature/deploy - Completed tasks:
    
    - modified github actions files
    
    * feature/deploy - Completed tasks:
    
    - modified and delete github actions files
    
    * Fixed runtime
    
    * fixed Procfile
    
    * Completed tasks:
    
    - adding testing folder
    - modified push action
    - adding setting test file
    
    * Complete tasks:
    
    - changed actions
    
    * Completed tasks:
    
    - add dependencies
    - change config

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn django-libraryapp.wsgi --log-file -
+web: gunicorn base.wsgi --log-file -
--- base/settings_test.py
+++ base/settings_test.py
@@ -82,13 +82,14 @@ WSGI_APPLICATION = "base.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "testing",
+        "USER": "postgres",
+        "PASSWORD": "postgres",
+        "PORT": 5432,
+        "HOST": "localhost",
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES["default"].update(db_from_env)
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
